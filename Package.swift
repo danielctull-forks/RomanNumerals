03e3aeb997e0a62e95a9cb6c4d59be0608af0763
Daniel Tull <dt@danieltull.co.uk>
2020-05-11 21:15:25 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,17 +6,9 @@ let package = Package(
     name: "RomanNumerals",
     products: [
         .library(name: "RomanNumerals", targets: ["RomanNumerals"]),
-        .executable(name: "roman", targets: ["RomanNumeralsTool"])
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
     ],
     targets: [
         .target(name: "RomanNumerals"),
         .testTarget(name: "RomanNumeralsTests", dependencies: ["RomanNumerals"]),
-        .target(name: "RomanNumeralsTool", dependencies: [
-            "RomanNumerals",
-            "ArgumentParser"
-        ])
     ]
 )
